86e9d88ba6ba65179233b13bbd7ca087652314af
zannathi <m.zannathi.k@gmail.com>
2023-12-06 07:45:47 +0600

    splash screen done

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:craftybay_ecommerce/ui/utils/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,8 +8,24 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Image.asset('')
+          Expanded(child: Center(child: SvgPicture.asset('assets/images/logo.svg', width: 150,))),
+          Column(
+            children: const [
+              CircularProgressIndicator(
+                color: primaryColor,
+              ),
+              Padding(padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Version 1.0',
+                style: TextStyle(
+                  color: greyColor, fontSize: 12, letterSpacing: 0.6,
+                ),
+              ),
+              ),
+            ],
+          ),
         ],
       ),
     );
---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+const Color primaryColor = Color(0xFF07ADAE);
+const Color greyColor = Color(0xFF757979);
+const Color blackColor = Color(0xFF000000);
