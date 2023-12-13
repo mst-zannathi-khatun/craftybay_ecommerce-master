46eb857b7d5d5f595c5aee9e3bca5aad29b5f161
zannathi <m.zannathi.k@gmail.com>
2023-12-13 08:57:35 +0600

    network caller code added

---
+++ lib/data/services/networkcaller.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:craftybay_ecommerce/data/models/response_model.dart';
+import 'package:craftybay_ecommerce/data/utils/urls.dart';
+import 'package:http/http.dart';
+
+class NetWorkCaller {
+  NetWorkCaller._();
+
+  static Future<ResponseModel> getRequest({required String url}) async {
+    try {
+      final response = await get(Uri.parse(Urls.baseUrl + url));
+      if (response.statusCode == 200) {
+        return ResponseModel(
+            statusCode: response.statusCode,
+            isSuccess: true,
+            returenData: jsonDecode(response.body));
+      } else {
+        return ResponseModel(
+            statusCode: response.statusCode,
+            isSuccess: false,
+            returenData: jsonDecode(response.body));
+      }
+    } catch (e) {
+      return ResponseModel(
+          statusCode: -1, isSuccess: false, returenData: e.toString());
+    }
+  }
+}
