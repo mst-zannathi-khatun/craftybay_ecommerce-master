8557ae741f55566a77f8729fc37b18e1603c67a7
zannathi <m.zannathi.k@gmail.com>
2023-12-13 09:14:28 +0600

    network caller code Added

--- lib/ui/screens/emailverification_screen.dart
+++ lib/ui/screens/emailverification_screen.dart
@@ -9,6 +9,7 @@ class EmailVerificationScreen extends StatefulWidget {
   @override
   State<EmailVerificationScreen> createState() => _EmailVerificationScreenState();
 }
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
   @override
@@ -16,23 +17,26 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset('assets/images/logo.svg', height: 100, width: 100,),
-            const SizedBox(height: 16,),
-            const Text("Welcome Back", style: TextStyle(letterSpacing: 0.5, fontSize: 30, color: blackColor),),
-            const SizedBox(height: 4,),
-            const Text("Please Enter Your Email Address", style: TextStyle(fontSize: 14, color: blackColor),),
-            const SizedBox(height: 16,),
-            const TextFromFieldWidget(
-              hintText: 'Email Address',
-            ),
-            const SizedBox(height: 8,),
-            const ElevateButtonWidget(text: "Next",),
-            const SizedBox(height: 16,),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset('assets/images/logo.svg', height: 100, width: 100,),
+              const SizedBox(height: 16,),
+              const Text("Welcome Back", style: TextStyle(letterSpacing: 0.5, fontSize: 30, color: blackColor),),
+              const SizedBox(height: 4,),
+              const Text("Please Enter Your Email Address", style: TextStyle(fontSize: 14, color: blackColor),),
+              const SizedBox(height: 16,),
+              const TextFromFieldWidget(
+                hintText: 'Email Address',
+              ),
+              const SizedBox(height: 8,),
+              const ElevateButtonWidget(text: "Next",),
+              const SizedBox(height: 16,),
+            ],
+          ),
         ),
       ),
     );
